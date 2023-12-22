bb5ee41d652650672166957c851f104f4bbd6d8f
VoxTerrain <154274770+VoxTerrain@users.noreply.github.com>
2023-12-22 13:09:50 +0200

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
-wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
+curl -s https://ngrok-agent.s3.amazonaws.com/ngrok.asc | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" | sudo tee /etc/apt/sources.list.d/ngrok.list && sudo apt update && sudo apt install ngrok
 tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XWvdIQegnz7fRUXkMvJWVy7Z5p_3SijirFvadrRgBJot32fG #Put Yours here
-./ngrok http 8080
+ngrok config add-authtoken 2X5RK3WdZ76CjhisSUtcFmPEjLI_6qw7sPSn84yuJ2ouGsETf #Put Yours here
+ngrok http 8080
